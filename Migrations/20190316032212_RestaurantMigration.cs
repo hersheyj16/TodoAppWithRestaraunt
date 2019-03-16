78f6ddb0499852a2b765ecf5a2aac531263e95a8
J Hershey <hersheyj32@gmail.com>
2019-03-15 20:25:52 -0700

    database migration

---
+++ Migrations/20190316032212_RestaurantMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotNetCoreSqlDb.Migrations
+{
+    public partial class RestaurantMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Restaurants",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ImageUrl = table.Column<string>(nullable: true),
+                    Phone = table.Column<string>(nullable: true),
+                    Url = table.Column<string>(nullable: true),
+                    Rating = table.Column<float>(nullable: false),
+                    Address = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurants", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Restaurants");
+        }
+    }
+}
---
+++ Models/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace DotNetCoreSqlDb.Models
+{
+    public class Restaurant
+    {
+        public int ID { get; set; }
+        public string ImageUrl { get; set; }
+        public string Phone { get; set; }
+        public string Url { get; set; }
+        public float Rating { get; set; }
+        double Latitude { get; set; }
+        double Longitude { get; set; }
+        public string Address { get; set; }
+    }
+}
